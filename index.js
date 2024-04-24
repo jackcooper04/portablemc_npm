91b7092832493016f4297db9e88d30b4dce43d35
Jack Cooper <jc3053765@gmail.com>
2024-04-24 17:01:42 +0100

    sync

--- index.js
+++ index.js
@@ -2,83 +2,124 @@
 const fs = require('fs');
 const path = require('path');
 const { Console } = require('console');
+const homedir = require('os').homedir();
+var logPath = path.join(__dirname, 'logs');
+var AUTHENTICATED_USER = {};
+var portableMCLocation = 'portablemc';
 
-var logPath = path.join(__dirname,'logs');
-
-var portableMCLocation = path.join(__dirname, 'portablemc.exe');
-
-console.log(portableMCLocation)
-
-async function executeMC(params,detached) {
-  const logOutput = fs.createWriteStream(path.join(logPath,'latest.log'));
-  const logger = new Console({ stdout: logOutput });
-  if (detached == undefined) {
-    detached = false;
-  }
-
-  const { spawn } = require('node:child_process');
-  const exe = spawn('cmd.exe', ['/C', portableMCLocation, ...params],
-    {
-      stdio: 'pipe'
+async function executeMC(params, detached) {
+  return new Promise((resolve) => {
+    const logOutput = fs.createWriteStream(path.join(logPath, 'latest.log'));
+    const logger = new Console({ stdout: logOutput });
+    if (detached == undefined) {
+      detached = false;
     }
-  )
 
-  if (detached) {
-    const viewer = spawn('cmd.exe', ['/C','node',path.join(__dirname,'loggerViewer.js')],
-    {
-      detached: true,
+    const { spawn } = require('node:child_process');
+    const exe = spawn('cmd.exe', ['/C', portableMCLocation, ...params],
+      {
+        stdio: 'pipe'
+      }
+    )
+
+    if (detached) {
+      const viewer = spawn('cmd.exe', ['/C', 'node', path.join(__dirname, 'loggerViewer.js')],
+        {
+          detached: true,
+        }
+      )
     }
-  )
-  }
-  exe.on('exit',function(code){
-    console.log('child exit code (spawn)', code);
-    let filename = `${(new Date().toJSON().slice(0,19))}.log`.replace(/:/g, ";");
+    exe.on('exit', function (code) {
+      console.log('child exit code (spawn)', code);
+      let filename = `${(new Date().toJSON().slice(0, 19))}.log`.replace(/:/g, ";");
 
-    let writer = fs.createWriteStream(path.join(logPath,filename));
-    writer.write(fs.readFileSync(path.join(logPath,'latest.log')));
+      let writer = fs.createWriteStream(path.join(logPath, filename));
+      writer.write(fs.readFileSync(path.join(logPath, 'latest.log')));
 
-    fs.writeFileSync(path.join(logPath,'latest.log'),'');
-    
-  })
+      fs.writeFileSync(path.join(logPath, 'latest.log'), '');
+      resolve(true);
 
-  exe.stdout.on('data', (data) => {
-    console.log(data);
-    logger.log(data.toString().replace(/(\r\n|\n|\r)/gm, ""));
+    })
+
+    exe.stdout.on('data', (data) => {
+      console.log(data);
+      logger.log(data.toString().replace(/(\r\n|\n|\r)/gm, ""));
+    });
+
+
+    exe.stderr.on('data', (data) => {
+      logger.log(data.toString().replace(/(\r\n|\n|\r)/gm, ""));
+      //console.error(data.toString());
+    });
   });
 
-
-  exe.stderr.on('data', (data) => {
-   //console.error(data.toString());
-  });
 }
 
+async function authenticate(email) {
+  return new Promise(async (resolve) => {
+  var minecraftDIR = path.join(homedir, 'AppData', 'Roaming', '.minecraft');
+  if (fs.existsSync(minecraftDIR)) {
+    var authFile = path.join(minecraftDIR, 'portablemc_auth.json');
+    if (fs.existsSync(authFile)) {
+      var authData = JSON.parse(fs.readFileSync(authFile));
+      var loggedUsers = authData.microsoft.sessions;
+      if (loggedUsers[email]) {
+        var authObj = {
+          username:loggedUsers[email].username,
+          uuid:loggedUsers[email].uuid,
+          email:email.replace(/(\w{3})[\w.-]+@([\w.]+\w)/, "$1***@$2")
+        };
+        console.log('user exists')
+        AUTHENTICATED_USER = authObj;
+        resolve(authObj);
+        return;
+      };
+    };
+  };
+  const userLoggedIn = await loginBrowser(email);
+  var authData = JSON.parse(fs.readFileSync(authFile));
+      var loggedUsers = authData.microsoft.sessions;
+  resolve(loggedUsers);
+  })
+}
 
 async function loginBrowser(email) {
-  const login = await executeMC(['login','--auth-service','microsoft',email]);
+  return new Promise(async (resolve) => {
+    const login = await executeMC(['login', '--auth-service', 'microsoft', email],true);
+    resolve(true);
+  });
+  //   setTimeout(function(){
+  //    return true;
+  //  }, 2000);
+  //
 };
 
 async function logout(email) {
-  const logout = await executeMC(['logout','--auth-service','microsoft',email]);
+  const logout = await executeMC(['logout', '--auth-service', 'microsoft', email]);
 };
 
-async function startGame(email,version,loader,logs) {
+async function startGame(email, version, loader, logs) {
   if (!version) {
     version = 'release'
   };
 
   if (!loader) {
-    const start = await executeMC(['start',version,'-l',email],logs);
+    const start = await executeMC(['start', version, '-l', email], logs);
   } else {
-    const start = await executeMC(['start',loader+":"+version,'-l',email]);
+    const start = await executeMC(['start', loader + ":" + version, '-l', email]);
   }
- 
+
 };
 
-startGame('jc3053765@gmail.com','1.7.10')
-
+//startGame('jc3053765@gmail.com','1.8','',true)
+async function test() {
+  const test1 = await authenticate('jackmeta004@gmail.com')
+  console.log(test1)
+}
+test();
 async function declareLogFile(path) {
   logPath = path;
 };
 
 
-module.exports = {startGame,loginBrowser,logout}
+module.exports = { startGame, loginBrowser, logout, declareLogFile }
